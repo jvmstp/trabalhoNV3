21682a1dfbe608648a27c341b3802434bfae9f8b
jvmstp <147636667+jvmstp@users.noreply.github.com>
2024-05-17 16:57:33 -0300

    Update [codEditora].ts

--- livros-next/pages/api/editoras/[codEditora].ts
+++ livros-next/pages/api/editoras/[codEditora].ts
@@ -1,22 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import  ControleEditora  from '../../../classes/controle/ControleEditora'
+import { NextApiRequest, NextApiResponse } from "next"
+import  controleEditora  from "../../../classes/controle/ControleEditora"
 
-const controleEditora = new ControleEditora()
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const { codEditora } = req.query
-
-        switch (req.method) {
-            case 'GET':
-                const nomeEditora = controleEditora.getNomeEditora(Number(codEditora))
-                res.status(200).json({ nome: nomeEditora })
-                break
-            default:
-                res.setHeader('Allow', ['GET'])
-                res.status(405).end(`Method ${req.method} Not Allowed`)
-        }
-    } catch (e) {
-        res.status(500).json({ statusCode: 500, message: onmessage })
-    }
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const editoras = controleEditora.getEditoras()
+    res.status(200).json(editoras)
+  } else {
+    res.status(405).json({ message: "Método não permitido" })
+  }
 }
