4206577f7809b9a89a0138c5f701474409e5940e
jvmstp <147636667+jvmstp@users.noreply.github.com>
2024-05-17 01:29:04 -0300

    Update index.tsx

--- livros-next/pages/index.tsx
+++ livros-next/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Menu } from '../componentes/Menu';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Menu.module.css';
 
 export default function Home() {
   return (
